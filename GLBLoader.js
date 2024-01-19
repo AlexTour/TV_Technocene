ca2b51a61c22cf586c843f8d9279479c7affdd25
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2024-01-19 12:09:12 +0100

    Update GLBLoader.js

--- GLBLoader.js
+++ GLBLoader.js
@@ -1,2 +1,2 @@
-document.getElementById('glb').src= "tvstaticNEWANIMATION.glb";
+document.getElementById('glb').src= "tvstaticNEWANIMATION_Unlit.glb";
 document.getElementById('glb').setAttribute("ios-src", "tvstaticNEWANIMATION.usdz");
